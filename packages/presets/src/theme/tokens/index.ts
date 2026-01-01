c54d998b38e21ab157c44962671a5e3a46d5c3b3
thisissakamoto <olanrewajuoladejo08@gmail.com>
2026-01-01 17:48:21 -0500

    chore(presets): add entrypoint file

---
+++ packages/presets/src/theme/tokens/index.ts
@@ -0,0 +1,47 @@
+import { animations } from "./animations";
+import { aspectRatios } from "./aspect-ratios";
+import { blurs } from "./blurs";
+import { borders } from "./borders";
+import { colors } from "./colors";
+import { cursor } from "./cursor";
+import { durations } from "./durations";
+import { easings } from "./easings";
+import { fontSizes } from "./font-sizes";
+import { fontWeights } from "./font-weights";
+import { fonts } from "./fonts";
+import { letterSpacings } from "./letter-spacings";
+import { lineHeights } from "./line-heights";
+import { radii } from "./radii";
+import { sizes } from "./sizes";
+import { spacing } from "./spacing";
+import { zIndex } from "./z-index";
+
+
+export const tokens = {
+  aspectRatios,
+  animations,
+  blurs,
+  borders,
+  durations,
+  easings,
+  fonts,
+  fontSizes,
+  fontWeights,
+  letterSpacings,
+  lineHeights,
+  radii,
+  spacing,
+  sizes,
+  zIndex,
+  cursor,
+  colors
+};
+
+
+import { colors as semanticColors } from "./semantic-colors";
+import { shadows } from "./shadows";
+
+export const semanticTokens = {
+  shadows,
+  colors: semanticColors,
+}
