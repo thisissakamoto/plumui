18ffa5b6d86242cce6c7fd12f348d14d9c5b1b46
thisissakamoto <olanrewajuoladejo08@gmail.com>
2026-01-01 17:45:13 -0500

    chore(presets): add font weights token

---
+++ packages/presets/src/theme/tokens/font-weights.ts
@@ -0,0 +1,31 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const fontWeights = defineTokens.fontWeights({
+  "100": {
+    value: "100",
+  },
+  "200": {
+    value: "200",
+  },
+  "300": {
+    value: "300",
+  },
+  "400": {
+    value: "400",
+  },
+  "500": {
+    value: "500",
+  },
+  "600": {
+    value: "600",
+  },
+  "700": {
+    value: "700",
+  },
+  "800": {
+    value: "800",
+  },
+  "900": {
+    value: "900",
+  },
+});
