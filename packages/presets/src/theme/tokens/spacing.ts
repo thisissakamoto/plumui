f4ba89cc9741aea1c87012eea9b2032eb5214db5
thisissakamoto <olanrewajuoladejo08@gmail.com>
2026-01-01 17:47:34 -0500

    chore(presets): add spacing token

---
+++ packages/presets/src/theme/tokens/spacing.ts
@@ -0,0 +1,82 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const spacing = defineTokens.spacing({
+  "auto": {
+    value: "auto",
+  },
+  "0": {
+    value: "0px",
+  },
+  "1": {
+    value: "1px",
+  },
+  "2": {
+    value: "2px",
+  },
+  "4": {
+    value: "4px",
+  },
+  "6": {
+    value: "6px",
+  },
+  "8": {
+    value: "8px",
+  },
+  "10": {
+    value: "10px",
+  },
+  "12": {
+    value: "12px",
+  },
+  "16": {
+    value: "16px",
+  },
+  "20": {
+    value: "20px",
+  },
+  "24": {
+    value: "24px",
+  },
+  "32": {
+    value: "32px",
+  },
+  "40": {
+    value: "40px",
+  },
+  "56": {
+    value: "56px",
+  },
+  "64": {
+    value: "64px",
+  },
+  "80": {
+    value: "80px",
+  },
+  "96": {
+    value: "96px",
+  },
+  "120": {
+    value: "120px",
+  },
+  "144": {
+    value: "144px",
+  },
+  "160": {
+    value: "160px",
+  },
+  "192": {
+    value: "192px",
+  },
+  "224": {
+    value: "224px",
+  },
+  "256": {
+    value: "256px",
+  },
+  "288": {
+    value: "288px",
+  },
+  "320": {
+    value: "320px",
+  },
+});
