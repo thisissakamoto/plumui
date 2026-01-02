214143a7bb23d3112ef862689a58949667d95474
thisissakamoto <olanrewajuoladejo08@gmail.com>
2026-01-02 05:19:17 -0500

    chore(react): refactor icon component

--- packages/react/src/components/icon/icon.ts
+++ packages/react/src/components/icon/icon.ts
@@ -1,11 +1,12 @@
 import * as IconifyIcon from "@iconify/react";
 import { styled } from "@plumui/styled/jsx";
+import { iconRecipe } from "@plumui/styled/recipes";
 import type { ComponentProps } from "@plumui/styled/types";
 
 export type IconProps = ComponentProps<typeof Icon>;
-export const Icon = styled(IconifyIcon.Icon);
+export const Icon = styled(IconifyIcon.Icon, iconRecipe);
 Icon.displayName = "Icon";
 
 export type InlineIconProps = ComponentProps<typeof InlineIcon>;
-export const InlineIcon = styled(IconifyIcon.Icon);
+export const InlineIcon = styled(IconifyIcon.Icon, iconRecipe);
 InlineIcon.displayName = "InlineIcon";
