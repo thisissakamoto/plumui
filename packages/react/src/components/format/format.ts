13284495fb1539b237df866998c599727a2a4471
thisissakamoto <olanrewajuoladejo08@gmail.com>
2026-01-06 12:12:02 -0500

    chore(react): refactor format component

--- packages/react/src/components/format/format.ts
+++ packages/react/src/components/format/format.ts
@@ -1,17 +1,15 @@
-import { ark } from '@ark-ui/react';
+import { Format } from '@ark-ui/react'
 import { styled } from '@plumui/styled/jsx';
 import type { ComponentProps } from '@plumui/styled/types';
 
 export type ByteProps = ComponentProps<typeof Byte>;
-export const Byte = styled(ark.span);
+export const Byte = styled(Format.Byte);
 Byte.displayName = 'FormatByte';
 
-// Number Component
-export type NumberProps = ComponentProps<typeof Number>;
-export const Number = styled(ark.span);
-Number.displayName = 'FormatNumber';
+export type IntegerProps = ComponentProps<typeof Integer>;
+export const Integer = styled(Format.Number);
+Integer.displayName = 'FormatInteger';
 
-// RelativeTime Component
 export type RelativeTimeProps = ComponentProps<typeof RelativeTime>;
-export const RelativeTime = styled(ark.span);
+export const RelativeTime = styled(Format.RelativeTime);
 RelativeTime.displayName = 'FormatRelativeTime';
